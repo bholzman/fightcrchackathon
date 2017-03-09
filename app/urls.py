2cd53bb1b6f10fd0a362718123aa40f49a9d0182
Ben Holzman <bholzman@flatiron.com>
2017-03-09 18:46:37 -0500

    implement trial has closed email functionality

--- app/urls.py
+++ app/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     url(r'^$', fightcrctrials.views.index, name='index'),
     url(r'^db', fightcrctrials.views.db, name='db'),
     url(r'^welcome/', fightcrctrials.views.welcome, name='welcome'),
+    url(r'^send-trial-closed-email/', fightcrctrials.views.send_trial_closed_email),
     url(r'^admin/', include(admin.site.urls))
 ]
--- fightcrctrials/views.py
+++ fightcrctrials/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 import json
+import os
+import sendgrid
+from sendgrid.helpers.mail import Email, Content, Mail
+
 from .models import Greeting, CRCTrials
 
+
 # Create your views here.
+@ensure_csrf_cookie
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html', {'trials': json.dumps(CRCTrials().trials)})
@@ -22,3 +29,20 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+
+def send_trial_closed_email(request):
+    if request.method == 'POST':
+        drug_name = request.POST.get('drug')
+        nct = request.POST.get('nct', 'No NCT# available')
+        user = request.POST.get('user')
+
+        sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+        from_email = Email("fightcrchackathon@gmail.com")
+        subject = 'User reports trial "{}" has closed'.format(drug_name)
+        to_email = Email(os.environ.get('CLOSED_TRIAL_EMAIL'))
+        message = "User '{}' reports that trial '{}' (NCT #: {}) has closed.".format(
+            user, drug_name, nct)
+        content = Content("text/plain", message)
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        return HttpResponse(response.body)
