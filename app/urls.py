1bb5f39becf419e28c8d46b1d95340a0b65202c2
Ben Holzman <bholzman@flatiron.com>
2017-03-10 09:46:14 -0500

    add favicon, fix prior io ok in detail view

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 admin.autodiscover()
 
 import fightcrctrials.views
@@ -14,5 +15,6 @@ urlpatterns = [
     url(r'^db', fightcrctrials.views.db, name='db'),
     url(r'^welcome/', fightcrctrials.views.welcome, name='welcome'),
     url(r'^send-trial-closed-email/', fightcrctrials.views.send_trial_closed_email),
+    url(r'^favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', include(admin.site.urls))
 ]
