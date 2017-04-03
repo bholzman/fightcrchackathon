1eafb210ccac0924d886380dc8d2d5f85fa0489d
Ben Holzman <bholzman@flatiron.com>
2017-04-03 18:13:46 -0400

    add stripped version of user text

--- fightcrctrials/templatetags/user_text.py
+++ fightcrctrials/templatetags/user_text.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.html import strip_tags
 from ..models import UserText
 
 register = template.Library()
@@ -8,5 +9,12 @@ register = template.Library()
 def tag(name):
     try:
         return mark_safe(UserText.objects.get(tag=name).text)
+    except:
+        return "<<{}>>".format(name)
+
+@register.simple_tag
+def tag_stripped(name):
+    try:
+        return strip_tags(UserText.objects.get(tag=name).text)
     except:
         return "<<{}>>".format(name)
